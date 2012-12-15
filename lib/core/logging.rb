f330c30def44aa212fb1f8fdae84c6fccf61262c
noxgirl <xoeverlux@gmail.com>
2012-12-15 16:49:32 -0700

    Core::Logging

---
+++ lib/core/logging.rb
@@ -0,0 +1,89 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+
+# Entering namespace: Core
+module Core
+
+  # Class Logging: Core logging functionality.
+  class Logging
+
+    # Create a new instance of Core::Logging.
+    # ()
+    def initialize
+
+      # Set our status to good.
+      @status = :good
+
+      # Create log/ if it's missing.
+      unless Dir.exists?('logs/')
+        begin
+          Dir.mkdir('logs/')
+        rescue => e
+          @status = :bad
+          $m.error("Could not create logs/: #{e}", true, e.backtrace)
+          return
+        end
+      end
+
+    end
+
+    # Log message of type ERROR.
+    # (str)
+    def error(msg)
+      log('ERROR', msg)
+    end
+
+    # Log message of type WARNING.
+    # (str)
+    def warning(msg)
+      log('WARNING', msg)
+    end
+
+    # Log message of type INFO.
+    # (str)
+    def info(msg)
+      log('INFO', msg)
+    end
+
+    # Log message of type DEBUG.
+    # (str)
+    def debug(msg)
+      log('DEBUG', msg)
+    end
+
+    #######
+    private
+    #######
+
+    # Log a message.
+    # (str, str)
+    def log(type, msg)
+
+      # If our status is bad, ignore the logging request.
+      return if @status == :bad
+      
+      # Create log/ if it's missing.
+      unless Dir.exists?('logs/')
+        begin
+          Dir.mkdir('logs/')
+        rescue => e
+          @status = :bad
+          $m.error("Could not create logs/: #{e}", true, e.backtrace)
+          return
+        end
+      end
+
+      # Open the file in the format of log/YYYYMMDD.log.
+      time = Time.now
+      File.open("logs/#{time.strftime('%Y%m%d')}.log", 'a') do |io|
+        # Append the message.
+        io.puts "[#{time.strftime('%Y-%m-%d %H:%M:%S %z')}] [#{type}] #{msg}"
+      end
+
+    end # def log
+
+  end # class Logging
+
+end # module Core
+
