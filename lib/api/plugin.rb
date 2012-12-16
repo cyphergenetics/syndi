576723dad91878c5ea05cd701e167b5b73154d3f
noxgirl <xoeverlux@gmail.com>
2012-12-16 04:08:53 -0700

    API::Plugin superclass

---
+++ lib/api/plugin.rb
@@ -0,0 +1,32 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+
+# Namespace: API
+module API
+  
+  # Class: Plugin
+  # Intended as a superclass for plugins
+  class Plugin
+
+    attr_reader :_name, :_version, :_summary, :_deps
+
+    def initialize(name, version, summary, deps)
+      @_name = name
+      @_version = version
+      @_summary = summary
+      deps.each do |dep|
+        unless $m.mods.include?(dep)
+          raise RuntimeError, "#@name cannot load because dependency '#{dep}' is missing"
+        end
+      end
+      @_deps = deps
+    end
+
+    def to_s
+      @_name
+    end
+
+  end # class Plugin
+
+end # module API
