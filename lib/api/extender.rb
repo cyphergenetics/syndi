224e1fc3abff3ff8c56d31b4b201a8d9f04054bf
noxgirl <xoeverlux@gmail.com>
2012-12-16 11:45:04 -0700

    API::Extender: prevent undesirable plugins

--- lib/api/extender.rb
+++ lib/api/extender.rb
@@ -44,12 +44,23 @@ module API
           eval "piclass = #{match[1]}.new", nil, ">/dev/null"
         rescue RuntimeError => e
           $m.error("Unable to load plugin #{piname} owing to RuntimeError!", false, e)
+          return
         rescue => e
           $m.error("Unable to load plugin #{piname} owing to an unknown exception!", false, e)
+          return
         end
       else
         $m.error("Unable to load plugin #{piname} because I cannot find the plugin's main class!")
+        return
       end
+
+      # Ensure we don't have duplicates.
+      if @plugins.include? piclass._name
+        $m.error("Initializing #{piname}: plugin initialized, but duplicate (#{piclass._name}) exists! Forcing out of runtime...")
+        unload(piclass)
+        return
+      end
+
 
       # Append to the list of loaded plugins.
       @plugins[piclass._name][:object] = piclass
@@ -71,7 +82,7 @@ module API
       end
 
       # Call the private method.
-      unload(@plugins[piname][:object])
+      unload(@plugins[piname][:object], @plugins[piname)
 
     end
 
@@ -80,20 +91,24 @@ module API
     #######
 
     # Unload a plugin.
-    def unload(object)
+    def unload(object, oclass)
 
       # Foremost, attempt to call uninitialize()
+      name = object._name
       begin
         object.uninitialize
       rescue NameError => e
-        $m.error("Unloading plugin #{object._name}: uninitialize() method is missing! (NameError) Serious issues could occur.", false, e)
+        $m.error("Unloading plugin #{name}: uninitialize() method is missing! (NameError) Serious issues could occur.", false, e)
       rescue => e
-        $m.error("Unloading plugin #{object._name}: uninitialize() call raised an exception! Serious issues could occur.", false, e)
+        $m.error("Unloading plugin #{name}: uninitialize() call raised an exception! Serious issues could occur.", false, e)
       ensure
         # Whatever the outcome, we must destroy the plugin.
-        name = object._name
-        eval "undef #{@plugins[name][:class]}"
-        @plugins.delete(name)
+        eval "undef #{oclass}"
+        if @plugins.include? name
+          if @plugins[name][:object] == object
+            @plugins.delete(name)
+          end
+        end
         $m.events.call('bot:onUnloadPlugin', name)
       end
     
