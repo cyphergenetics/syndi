d1c8b573c8c73b36259ddef798bc66bcc60d333c
noxgirl <xoeverlux@gmail.com>
2012-12-22 04:00:22 -0700

    Improvements to and YARD for API::Plugin.

--- lib/api/plugin.rb
+++ lib/api/plugin.rb
@@ -2,38 +2,64 @@
 # Copyright (c) 2013, Auto Project
 # Distributed under the terms of the three-clause BSD license.
 
-################################################################
-# API::Plugin
-#
-# A class intended to operate as a basic superclass for plugins.
-################################################################
-
 # Namespace: API
 module API
   
-  # Class: Plugin
-  # Intended as a superclass for plugins
-  class Plugin
+  # A standard mixin for plugins to employ.
+  #
+  # @api Auto
+  # @since 4.0.0
+  # @author noxgirl
+  #
+  # @!attribute [r] name
+  #   @return [String] The name of the plugin.
+  #
+  # @!attribute [r] version
+  #   @return [Integer] The version of the plugin.
+  #
+  # @!attribute [r] summary
+  #   @return [String] A summary of the plugin.
+  #
+  # @!attribute [r] deps
+  #   @return [Array<String>] A list of modules upon which the plugin depends.
+  module Plugin
 
-    attr_reader :_name, :_version, :_summary, :_deps
+    attr_reader :name, :version, :summary, :deps
 
-    def initialize(name, version, summary, deps)
-      @_name = name
-      @_version = version
-      @_summary = summary
+    # Register the plugin's essential data, and check for presence of
+    # dependencies.
+    #
+    # @param [Array<String>] deps List of core modules necessary for operation.
+    #
+    # @yieldparam [self] c An object for configuration.
+    #
+    # @example
+    #   pi_register(['irc']) do |c|
+    #     c.name    = "Foobar plugin"
+    #     c.version = 2.1
+    #     c.summary = "A plugin for the art of foobar."
+    #   end
+    def self.pi_register(deps)
+      
+      @name    = name
+      @version = version
+      @summary = summary
+      # Yield for configuration.
+      yield self if block_given? or raise LoadError, "Plugin failed to register because it did not configure itself."
+
+      # Check dependencies.
       deps.each do |dep|
         unless $m.mods.include?(dep)
-          raise RuntimeError, "#@name cannot load because dependency '#{dep}' is missing"
+          raise LoadError, "#@name cannot load because dependency '#{dep}' is missing"
         end
       end
-      @_deps = deps
+      @deps    = deps
+    
     end
 
-    def to_s
-      @_name
-    end
+    def self.to_s; @name; end
 
-  end # class Plugin
+  end # module Plugin
 
 end # module API
 
