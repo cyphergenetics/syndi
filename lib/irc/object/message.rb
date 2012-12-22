1398c45ed052c2e66de6ecd90eb69e5fb6d3e454
noxgirl <xoeverlux@gmail.com>
2012-12-21 21:41:28 -0700

    Optimizations of IRC::Object::User; docs for that and IRC::Object::Message.

--- lib/irc/object/message.rb
+++ lib/irc/object/message.rb
@@ -8,13 +8,12 @@ module IRC
   # Namespace: Object
   module Object
     
-    # IRC::Object::Message
-    #
     # A class which represents an IRC message, its associated properties, and
     # offers methods for working with the message.
     #
     # @since 4.0.0
     # @author noxgirl
+    # @api IRC
     #
     # @!attribute [r] irc
     #   @return [IRC::Server] The server on which the message was received.
@@ -36,8 +35,6 @@ module IRC
 
       attr_reader :irc, :sender, :body, :nature, :channel
 
-      # @!method new(irc, sender, body, nature, channel)
-      #
       # Process a new message.
       #
       # @param [IRC::Server] irc The server on which the message was received.
@@ -45,6 +42,9 @@ module IRC
       # @param [String] body The body of the message.
       # @param [Symbol] nature The nature of the message: either a +:notice+ or a +:msg+.
       # @param [IRC::Object::Channel] channel If it was received through a channel, the channel.
+      #
+      # @example
+      #   message = IRC::Object::Message.new(irc, user, body, :msg, channel)
       def initialize(irc, sender, body, nature=:notice, channel=nil)
 
         @irc     = irc
@@ -58,13 +58,16 @@ module IRC
       # Reply to this message.
       #
       # @param [String] msg The message with which to reply.
-      # @param [true] in_channel If the response should be in-channel (assuming it was received in a channel)
-      # @param [false] in_channel If it should be private regardless of where it was received.
+      # @param [Boolean] in_channel If the response should be in-channel (assuming it was received in a channel), specify +true+.
+      #   If it should be private regardless of where it was received, specify +false+.
       #
       # @note Essentially reply() exists to simplify the API. 
       #   Rather than necessitating that commands use endless, illegible conditional
       #   nests to determine what to do, reply() is available so the API will just
       #   use some common sense to do it for them.
+      #
+      # @example
+      #   msg.reply("The bar is of foo, indeed.", true)
       #
       # @todo Unfinished.
       def reply(msg, in_channel)
--- lib/irc/object/user.rb
+++ lib/irc/object/user.rb
@@ -2,24 +2,52 @@
 # Copyright (c) 2013, Auto Project
 # Distributed under the terms of the three-clause BSD license.
 
-#########################################
-# IRC::Object::User
-#
-# A class which represents a user on IRC.
-#########################################
-
 # Namespace: IRC
 module IRC
 
   # Namespace: Object
   module Object
 
-    # Class: User
+    # A class which represents an individual user on IRC, and which provides
+    # useful data regarding said user.
+    #
+    # @since 4.0.0
+    # @api IRC
+    # @author noxgirl
+    #
+    # @!attribute nick
+    #   @return [String] The user's nickname.
+    #
+    # @!attribute user
+    #   @return [String] If known, the user's username or ident.
+    #   @return [nil] If unknown.
+    #   @see #user_known?
+    #
+    # @!attribute host
+    #   @return [String] If known, the user's hostname or mask.
+    #   @return [nil] If unknown.
+    #   @see #host_known?
+    #
+    # @!attribute account
+    #   @return [String] If the user is logged in, their account name.
+    #   @see #logged_in?
+    #
+    # @!attribute away
+    #   @return [true] If the user is away.
+    #   @return [false] If the user is present.
     class User
 
-      attr_accessor :nick, :user, :host, :account
+      attr_reader :nick, :user, :host, :account, :away
 
-      # Create a new instance.
+      # Process a new user.
+      #
+      # @param [String] nickname The user's nickname.
+      # @param [String] username The user's username or ident.
+      # @param [String] hostname The user's hostname or mask.
+      # @param [Boolean] away Whether the user is away: +true+ or +false+.
+      #
+      # @example
+      #   user = IRC::Object::User.new('missfoo', 'cowmilk', 'the.night.is.lovely', false)
       def initialize(nickname, username=nil, hostname=nil, away=false)
 
         @nick    = nickname
@@ -30,58 +58,66 @@ module IRC
 
       end
 
-      # Process updates to information.
-      def update(nickname=nil, username=nil, hostname=nil, away=false)
-        
-        unless nickname.nil?
-          @nick = nickname
-        end
-
-        unless username.nil?
-          @user = username
-        end
-
-        unless hostname.nil?
-          @host = hostname
-        end
-
-        @away = away
-      
-      end
-
-      # Process a login.
+      # If the user logs into an account, this is used to specify the name of
+      # the account with which ze has identified.
+      #
+      # @param [String] accountname The name of the account.
+      #
+      # @example
+      #   user.login('root')
       def login(accountname)
         unless accountname.nil?
           @account = accountname
         end
       end
 
-      # Process a logout.
+      # If the user logs out of hir account, this is used to update their
+      # logged-in status.
       def logout
         @account = nil
       end
 
-      # away= is special in that it must be a boolean.
+      # Update the user's known away status.
+      #
+      # @param [Boolean] new The user's new away status, which should be +true+ or +false+.
       def away=(new)
         if new == true or new == false
           @away = new
         end
       end
+      
+      # Update the user's known hostname or mask.
+      #
+      # @param [String] new The user's new hostname.
+      def host=(new); @host = new unless new.nil?; end
 
-      # Check if their username is known.
-      def user_known?
-        @user.nil? ? false : true
-      end
+      # Update the user's known nickname.
+      #
+      # @param [String] new The user's new nickname.
+      def nick=(new); @nick = new unless new.nil?; end
+      
+      # Update the user's known username or ident.
+      #
+      # @param [String] new The user's new username.
+      def user=(new); @user = new unless new.nil?; end
 
-      # Check if their hostname is known.
-      def host_known?
-        @host.nil? ? false : true
-      end
-
-      # Check if they are logged in.
-      def logged_in?
-        @account.nil? ? false : true
-      end
+      # This will check whether the user's username is known.
+      #
+      # @return [true] If known.
+      # @return [false] If Unknown.
+      def user_known?; @user.nil? ? false : true; end
+      
+      # This will check whether the user's hostname is known.
+      #
+      # @return [true] If known.
+      # @return [false] If Unknown.
+      def host_known?; @host.nil? ? false : true; end
+      
+      # This will check whether the user is logged in.
+      #
+      # @return [true] If known.
+      # @return [false] If Unknown.
+      def logged_in?; @account.nil? ? false : true; end
 
     end # class User
 
