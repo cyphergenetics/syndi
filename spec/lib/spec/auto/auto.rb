ebdc548a0c21d6aadb0b759785342c57dbb928d5
noxgirl <xoeverlux@gmail.com>
2012-12-27 02:11:40 -0700

    mock @events in Spec::Auto::Auto

--- spec/lib/spec/auto/auto.rb
+++ spec/lib/spec/auto/auto.rb
@@ -2,13 +2,17 @@
 # Copyright (c) 2013, Auto Project
 # Distributed under the terms of the three-clause BSD license.
 
+require 'spec/auto/api'
+
 module Spec
   module Auto
     class Auto
+      attr_reader :events
       def initialize
         [:debug,:warn,:error,:log,:info,:foreground].each do |x| 
           define_singleton_method(x) {|*args| nil }
         end
+        @events = Spec::Auto::API::Events.new
       end
     end
   end
