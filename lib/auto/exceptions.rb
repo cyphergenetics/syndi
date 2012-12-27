3308c9b9c50d78ab84b98ce241268018c7a812d7
noxgirl <xoeverlux@gmail.com>
2012-12-26 20:51:17 -0700

    Exceptions

---
+++ lib/auto/exceptions.rb
@@ -0,0 +1,11 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+
+class Generic < StandardError
+end
+
+class ConfigError < Generic
+end
+
+# vim: set ts=4 sts=2 sw=2 et:
