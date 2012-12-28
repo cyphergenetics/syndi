b63b1b4695974cc71f9af57c5adf463b140a3910
noxgirl <xoeverlux@gmail.com>
2012-12-27 19:57:12 -0700

    events_spec: tests for Auto::API::Events; current result: 3 success, 1 fail

---
+++ spec/events_spec.rb
@@ -0,0 +1,45 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+require 'bacon'
+require 'spec/test_helpers'
+
+require 'auto/api/events'
+
+describe "The API event system" do
+
+  before do
+    @events = Auto::API::Events.new
+  end
+
+  it 'should return an ID after creating a new event with on()' do
+    @events.on(self, 'fooBar') { nil }.class.should.equal String
+  end
+
+  it 'should trigger all hooks of an event when said event is called' do
+    @ok = false
+    @events.on(self, 'bunnyEvent') { @ok = true }
+    @events.call('bunnyEvent')
+    @ok.should.be.true
+  end
+
+  it 'should trigger events in the order of highest to lowest priority' do
+    @order = ''
+    @events.on(self, 'kittenEvent', 5) { @order << 'C' }
+    @events.on(self, 'kittenEvent', 1) { @order << 'A' }
+    @events.on(self, 'kittenEvent', 3) { @order << 'B' }
+    @events.call('kittenEvent')
+    @order.should.equal 'ABC'
+  end
+
+  it 'should delete an event given its ID' do
+    @deleted = true
+    id = @events.on(self, 'unicornEvent') { @deleted = false }
+    @events.del(self, id)
+    @events.call('unicornEvent')
+    @deleted.should.be.true
+  end
+
+end
+
+# vim: set ts=4 sts=2 sw=2 et:
