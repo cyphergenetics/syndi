9ba21ffe976145598cf3f622c3da2c6ba927d065
noxgirl <xoeverlux@gmail.com>
2012-12-31 06:54:28 -0700

    More code for the timers_spec

--- spec/timers_spec.rb
+++ spec/timers_spec.rb
@@ -5,5 +5,20 @@
 require 'bacon'
 require 'spec/test_helpers'
 
+describe 'The API timer system' do
+
+  it 'should have a publicly viewable array of threads' do
+  end
+
+  it 'should correctly execute once-only timers' do
+  do
+
+  it 'and repeating timers' do
+  end
+
+  it 'should terminate a timer with #del'
+  end
+
+end
 
 # vim: set ts=4 sts=2 sw=2 et:
