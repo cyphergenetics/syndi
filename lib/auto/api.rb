0ee36a4c9f621fd823bb81538be166bd314ee964
noxgirl <xoeverlux@gmail.com>
2012-12-31 08:28:50 -0700

    Auto::API to simplify the API. [ci skip]

---
+++ lib/auto/api.rb
@@ -0,0 +1,8 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+
+require 'auto/api/events'
+require 'auto/api/timers'
+
+# vim: set ts=4 sts=2 sw=2 et:
