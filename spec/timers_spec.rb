2f60fb3e2671bb8e171b072bf4fbe7dc7904207f
noxgirl <xoeverlux@gmail.com>
2012-12-31 05:14:21 -0700

    Begin timers_spec

---
+++ spec/timers_spec.rb
@@ -0,0 +1,9 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the three-clause BSD license.
+
+require 'bacon'
+require 'spec/test_helpers'
+
+
+# vim: set ts=4 sts=2 sw=2 et:
