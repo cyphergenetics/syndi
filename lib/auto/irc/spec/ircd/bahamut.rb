ff3df92d226b08f13522c1c862400a703e01e838
noxgirl <xoeverlux@gmail.com>
2013-01-02 07:54:59 -0700

    Base specification, Auto::IRC::Spec::IRCd::Bahamut (subclass of Universal).

---
+++ lib/auto/irc/spec/ircd/bahamut.rb
@@ -0,0 +1,30 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+# namespace Auto
+module Auto
+
+  # namespace IRC
+  module IRC
+
+    # namespace Spec
+    module Spec
+
+      # namespace IRCd
+      module IRCd
+
+        # Specifications for Bahamut.
+        class Bahamut < Universal
+        
+        end # class Bahamut
+
+      end # module IRCd
+
+    end # module Spec
+
+  end # module IRC
+
+end # module Auto
+
+# vim: set ts=4 sts=2 sw=2 et:
