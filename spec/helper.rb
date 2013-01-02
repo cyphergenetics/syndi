ca813193dfa79d9deb22f2c65e667fc7fcbce7df
noxgirl <xoeverlux@gmail.com>
2013-01-02 10:57:16 -0700

    New spec/helper.

---
+++ spec/helper.rb
@@ -0,0 +1,8 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+require 'bacon'
+require 'facon'
+
+# vim: set ts=4 sts=2 sw=2 et:
