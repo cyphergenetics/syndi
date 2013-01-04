d171a23ae418446e5222fb06c878d7da4c637ef8
noxgirl <xoeverlux@gmail.com>
2013-01-04 08:54:44 -0700

    Fixed Auto::IRC::Object::Entity.

--- lib/auto/irc/object/entity.rb
+++ lib/auto/irc/object/entity.rb
@@ -17,17 +17,22 @@ module Auto
       #
       # @!attribute [r] irc
       #   @return [Auto::IRC::Server] The IRC server on which this entity exists.
+      #
+      # @!attribute [r] name
+      #   @return [String] The name of the entity.
       class Entity
 
-        attr_reader :irc
+        attr_reader :irc, :name
 
         # New instance.
         #
         # @param [Auto::IRC::Server] irc The server.
         # @param [Symbol] type Either +:channel+ or +:user+.
-        def initialize(irc, type)
+        # @param [String] name The name of this entity (nickname or channel name).
+        def initialize(irc, type, name)
           @irc         = irc
           @entity_type = type
+          @name        = name
         end
 
         # @return [true, false] Whether we're a channel.
@@ -51,27 +56,29 @@ module Auto
         def msg(message, notice=false)
           
           command = (notice == false ? 'PRIVMSG' : 'NOTICE')
-          len     = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} :".length
-          raw     = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} :#{message}\r\n"
+          len     = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} #@name :".length
+          raw     = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} #@name :#{message}\r\n"
 
           if raw.length > 512
             
             msgs     = []
             nmessage = message 
-            while raw.length > 512
+            until raw.length <= 512
               msgs << nmessage.slice!(0, 512-len)
-              raw = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} :#{nmessage}\r\n"
+              raw = ":#{@irc.nick}!#{@irc.user}@#{@irc.mask} #{command} #@name :#{nmessage}\r\n"
             end
             
-            msgs.each { |m| @irc.snd "#{command} :#{m}" }
+            msgs.each { |m| @irc.snd "#{command} #@name :#{m}" }
           
           else
-            @irc.snd "#{command} :#{message}"
+            @irc.snd "#{command} #@name :#{message}"
           end
 
           $m.events.call 'irc:onMsg', self, message
 
         end # def msg
+
+        def to_s; @name; end
 
       end # class Entity
 
