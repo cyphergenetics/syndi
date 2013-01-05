b3a7e54a0948ddc823f682bb6c9b2ade15c5455a
noxgirl <xoeverlux@gmail.com>
2013-01-04 17:02:22 -0700

    Finishing up Auto::DSL::Base [ci skip]

--- lib/auto/dsl/base.rb
+++ lib/auto/dsl/base.rb
@@ -5,34 +5,35 @@
 # Namespace: Auto
 module Auto
 
-  # Namespace: API
-  module API
+  # Namespace: DSL
+  module DSL
 
-    # Namespace: Helper
-    module Helper
+    # A domain-specific language (DSL) wrapper mixin for simple usage of the events
+    # system, {Auto::API::Events}, and the timers system, {Auto::API::Timers}.
+    #
+    # @api DSL
+    # @author noxgirl
+    # @since 4.0.0
+    #
+    # @see Auto::API::Events
+    # @see Auto::API::Timers
+    module Base
+        
+      # @see Auto::API::Timers#spawn
+      def clock_do(*args);   $m.clock.spawn(*args); end
+      # @see Auto::API::Timers#del
+      def clock_stop(*args); $m.clock.del(*args);   end
 
-      # A domain-specific language (DSL) wrapper mixin for simple usage of the events
-      # system, {Auto::API::Events}.
-      #
-      # @api DSL
-      # @author noxgirl
-      # @since 4.0.0
-      #
-      # @see Auto::API::Events
-      module Events
+      # @see Auto::API::Events#on
+      def ev_on(*args); $m.events.on(*args);   end
+      # @see Auto::API::Events#call
+      def ev_do(*args); $m.events.call(*args); end
+      # @see Auto::API::Events#del
+      def ev_rm(*args); $m.events.del(*args);  end
 
-        # @see Auto::API::Events#on
-        def ev_on(*args); $m.events.on(*args);   end
-        # @see Auto::API::Events#call
-        def ev_do(*args); $m.events.call(*args); end
-        # @see Auto::API::Events#del
-        def ev_rm(*args); $m.events.del(*args);  end
+    end # module Base
 
-      end # module Events
-
-    end # module Helper
-
-  end # module API
+  end # module DSL
 
 end # module Auto
 
