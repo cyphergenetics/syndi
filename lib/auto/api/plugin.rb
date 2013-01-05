5cd599a64297b4fd665f3b796bacb1fb9707e308
noxgirl <xoeverlux@gmail.com>
2013-01-04 17:09:32 -0700

    And so begins Auto::API::Plugin

---
+++ lib/auto/api/plugin.rb
@@ -0,0 +1,24 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+module Auto
+
+  module API
+
+    # A basic superclass for plugins.
+    #
+    # @api Auto
+    # @since 4.0.0
+    # @author noxgirl
+    # @author swarley
+    class Plugin
+
+
+    end # class Plugin
+
+  end # module API
+
+end # module Auto
+
+# vim: set ts=4 sts=2 sw=2 et:
