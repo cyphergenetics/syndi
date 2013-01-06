69438ba75e6c7ab2e1a5b3b8e419087b0f960721
noxgirl <xoeverlux@gmail.com>
2013-01-06 11:30:37 -0700

    Vastly simplified the starting process of Auto::Bot, with a new event :start.

--- lib/auto/bot.rb
+++ lib/auto/bot.rb
@@ -100,38 +100,9 @@ module Auto
 
     # Start the bot.
     def start
-
-      # Check if the irc core library is loaded.
-      if @libs.include?('irc')
-        # Prepare for incoming data.
-        $m.events.on(self, 'irc:onReadReady') do |irc|
-          until irc.recvq.length == 0
-            line = irc.recvq.shift.chomp
-            foreground("#{irc} >> #{line}")
-            @irc_parser.parse(irc, line)
-          end
-        end
-        
-        # Iterate through each IRC server in the config, and connect to them.
-        @conf.x['irc'].each do |name, hash|
-          begin
-            # Configure the IRC instance.
-            @irc_sockets[name] = IRC::Server.new(name) do |c|
-              c.address = hash['address']
-              c.port    = hash['port']
-              c.nick    = hash['nickname'][0] 
-              c.user    = hash['username']
-              c.real    = hash['realName']
-              c.ssl     = hash['useSSL']
-            end
-
-            # Connect.
-            @irc_sockets[name].connect
-          rescue => e
-            error("Connection to #{name} failed: #{e}", false, e.backtrace)
-          end
-        end
-      end
+      
+      # Call the start event.
+      @events.call :start
 
       # Throw the program into the main loop.
       debug("Producing a thread and entering the main loop...")
