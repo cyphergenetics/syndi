f70c7442a3537568eb3a4680a5d6d1115930a1db
noxgirl <xoeverlux@gmail.com>
2013-01-06 10:21:12 -0700

    Beginning IRC::Library

---
+++ lib/auto/irc/library.rb
@@ -0,0 +1,27 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+require 'irc/server'
+require 'irc/object/entity'
+require 'irc/object/channel'
+require 'irc/object/user'
+
+module Auto
+  
+  module IRC
+    
+    # The base of the IRC framework.
+    class Library
+
+      def initialize
+        
+      end
+
+    end
+
+  end
+
+end
+
+# vim: set ts=4 sts=2 sw=2 et:
