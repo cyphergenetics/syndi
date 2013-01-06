bd28cb0409a961897cc036a617736c77cbf67e66
noxgirl <xoeverlux@gmail.com>
2013-01-06 11:10:02 -0700

    Add the bangs to Ruby stdlib extensions

--- lib/auto/rubyext/string.rb
+++ lib/auto/rubyext/string.rb
@@ -5,10 +5,10 @@
 # This changes the String class in Ruby's standard library to make life easier
 # by aliasing String#uc to String#upcase and String#dc to String#downcase
 class String
-  # Alias 'uc' to 'upcase', for making life easier.
-  alias_method 'uc', 'upcase'
-  # And 'dc' to 'downcase'.
-  alias_method 'dc', 'downcase'
+  alias uc, upcase
+  alias dc, downcase
+  alias uc!, upcase!
+  alias dc!, downcase!
 end
 
 # vim: set ts=4 sts=2 sw=2 et:
