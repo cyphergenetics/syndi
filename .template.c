7f29f38bd59634d2f8bce653a3e6982084369f98
noxgirl <xoeverlux@gmail.com>
2013-01-07 14:25:34 -0700

    Here's a template for C source files. [ci skip]

---
+++ .template.c
@@ -0,0 +1,8 @@
+/*
+   Auto 4
+   Copyright (c) 2013, Auto Project. All rights reserved.
+   This is free software distributed under the terms of the FreeBSD license (see LICENSE.md).
+ */
+
+
+/* vim: set ts=4 sts=4 sw=4 et cindent: */
