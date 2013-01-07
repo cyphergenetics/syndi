f201b64d5b47d2176827cfa2f79e56468187243a
noxgirl <xoeverlux@gmail.com>
2013-01-06 18:49:23 -0700

    ok

---
+++ lib/auto/dsl/irc.rb
@@ -0,0 +1,14 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+module Auto
+  module DSL
+    module IRC
+      # ok
+    end
+  end
+end
+
+
+# vim: set ts=4 sts=2 sw=2 et:
