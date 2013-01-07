d477ad6e9a79db5213bfbc3fe2e1b464bcacba20
noxgirl <xoeverlux@gmail.com>
2013-01-06 17:25:32 -0700

    Fixing the String extension.

--- lib/auto/rubyext/string.rb
+++ lib/auto/rubyext/string.rb
@@ -5,10 +5,10 @@
 # This changes the String class in Ruby's standard library to make life easier
 # by aliasing String#uc to String#upcase and String#dc to String#downcase
 class String
-  alias uc, upcase
-  alias dc, downcase
-  alias uc!, upcase!
-  alias dc!, downcase!
+  alias_method :uc, :upcase
+  alias_method :dc, :downcase
+  alias_method :uc!, :upcase!
+  alias_method :dc!, :downcase!
 end
 
 # vim: set ts=4 sts=2 sw=2 et:
