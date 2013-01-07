28a20edb223ae320752da3098bccf725e7561f80
noxgirl <xoeverlux@gmail.com>
2013-01-06 22:01:43 -0700

    Auto::IRC::State::Support#cap [Array<String>]

--- lib/auto/irc/state/support.rb
+++ lib/auto/irc/state/support.rb
@@ -2,6 +2,29 @@
 # Copyright (c) 2013, Auto Project
 # Distributed under the terms of the FreeBSD license (LICENSE.md).
 
+module Auto
+  module IRC
+    module State
+      
+      # A state-management class which records the various capabilities of the
+      # IRC daemon.
+      #
+      # @!attribute cap
+      #   @return [Array<String>] A list of enabled capabilities.
+      class Support
 
+        attr_accessor :cap
+
+        def initialize
+          @cap = []
+        end
+
+      end # class Support
+
+    end # module State
+
+  end # module IRC
+
+end # module Auto
 
 # vim: set ts=4 sts=2 sw=2 et:
