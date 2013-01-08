cyphergenetics/syndi

commit 68d0ae534d73a556154a8bd9d817b162e999104f

Adding a mutual exclusion for thread safety. This fixes a bug that produced a number of error reports upon shutdown.

lib/auto/bot.rb
@@ -112,8 +112,9 @@ module Auto
       # Throw the program into the main loop.
       @events.threads.each { |thr| thr.join } # block until we're ready to go
       debug("Producing a thread and entering the main loop...") if @opts.verbose?
-      @netloop = Thread.new { main_loop }
-      @netloop.join
+      #@netloop = Thread.new { main_loop }
+      #@netloop.join
+      main_loop
 
     end
 
@@ -136,7 +137,7 @@ module Auto
 
         # Iterate through sockets ready for reading.
         ready_read.each do |socket|
-          @events.call :net_receive, assoc_objects[socket] unless socket.eof?
+          @events.call :net_receive, assoc_objects[socket]
         end
       end
     end
@@ -158,7 +159,7 @@ module Auto
       @log.error(msg)
 
       if fatal
-        @netloop.kill if @netloop.active
+        #@netloop.kill if @netloop.active
         exit 1
       end
     end

commit 78c995fb05745fe12f2674f49883e71cfee64f47

Fixing threading, which 68d0ae5 broke to some extent.

lib/auto/bot.rb
@@ -58,7 +58,7 @@ module Auto
     # Create a new instance of Auto.
     #
     # @param [Hash{String => Object}] opts A hash of options.
-    def initialize(opts)
+    def initialize opts
       # Save options.
       @opts = opts
       
@@ -112,9 +112,8 @@ module Auto
       # Throw the program into the main loop.
       @events.threads.each { |thr| thr.join } # block until we're ready to go
       debug("Producing a thread and entering the main loop...") if @opts.verbose?
-      #@netloop = Thread.new { main_loop }
-      #@netloop.join
-      main_loop
+      @netloop = Thread.new { main_loop }
+      @netloop.join
 
     end
 
@@ -147,7 +146,7 @@ module Auto
     # @param [String] msg The message.
     # @param [true, false] fatal Whether this error is fatal (will kill the program).
     # @param [Array<String>] bt Backtrace.
-    def error(msg, fatal=false, bt=nil)
+    def error msg, fatal = false, bt = nil
       # Print it to STDERR.
       STDERR.puts "ERROR: #{msg}".red
       unless bt.nil?