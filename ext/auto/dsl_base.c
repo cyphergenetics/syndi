8775b90c36c92ee9f588f0eb467d0d46fa3546c4
Matthew Carey <matthew.b.carey@gmail.com>
2013-01-09 00:50:03 -0500

    I guess there is no reason not to have this in C.

---
+++ ext/auto/dsl_base.c
@@ -0,0 +1,42 @@
+// vim: sts=4 ts=4 et
+#include "ruby.h"
+
+VALUE mAuto;
+VALUE mDSL;
+VALUE mBase;
+
+static VALUE clock_do(int argc, VALUE *argv, VALUE self)
+{
+    return rb_funcall2(rb_funcall(rb_funcall(rb_gv_get("$m"), rb_intern("clock"), 0), rb_intern("spawn"), 0), argc, argv);
+}
+
+static VALUE clock_stop(int argc, VALUE *argv, VALUE self)
+{
+    return rb_funcall2(rb_funcall(rb_funcall(rb_gv_get("$m"), rb_intern("clock"), 0), rb_intern("del"), 0), argc, argc);
+}
+
+static VALUE undo_on(VALUE self, VALUE sys, VALUE hook)
+{
+    if(sys == rb_intern("auto"))
+    {
+        return rb_funcall(rb_funcall(rb_gv_get("$m"), rb_intern("events"), 0) rb_intern("del"), 1, hook);
+    }
+    else
+    {
+        if(rb_funcall(rb_gv_get("$m"), rb_intern("respond_to?"), 1, sys) == Qtrue)
+        {
+            return rb_funcall(rb_funcall(rb_funcall(rb_gv_get("$m"), sys, 0), rb_intern("events"), 0), rb_intern("del"), 1, hook);
+        }
+        else
+        {
+            return Qnil;
+        }
+    }
+}
+
+void Init_dsl_base()
+{
+    mAuto = rb_define_module("Auto");
+    mDSL  = rb_define_module_under(mAuto, "DSL");
+    mBase = rb_define_module_under(mDSL, "Base");
+}
