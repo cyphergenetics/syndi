7e2c3302d01102c1749e733d650525aa16354eb6
noxgirl <xoeverlux@gmail.com>
2013-01-08 18:58:34 -0700

    Here's an extconf

---
+++ ext/auto/extconf.rb
@@ -0,0 +1,9 @@
+# Auto 4
+# Copyright (c) 2013, Auto Project
+# Distributed under the terms of the FreeBSD license (LICENSE.md).
+
+require 'mkmf'
+
+create_makefile 'auto/logger'
+
+# vim: set ts=4 sts=2 sw=2 et:
