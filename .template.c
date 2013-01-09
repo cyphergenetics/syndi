f69bf6ce694630b36a05f46c1573f7a0cb73deab
noxgirl <xoeverlux@gmail.com>
2013-01-08 17:02:51 -0700

    Err, cough. Fixing the C template. <.<

--- .template.c
+++ .template.c
@@ -1,7 +1,8 @@
 /*
-   Auto 4
-   Copyright (c) 2013, Auto Project. All rights reserved.
-   This is free software distributed under the terms of the FreeBSD license (see LICENSE.md).
+ * Auto 4
+ * Copyright (c) 2013, Auto Project. All rights reserved.
+ * This is free software distributed under the terms of the FreeBSD license (see LICENSE.md).
+ *
  */
 
 
