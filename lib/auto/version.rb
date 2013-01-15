23ec814466779d704ae6ef7baa42e322fae13173
noxgirl <autumn@destellae.net>
2013-01-14 17:02:19 -0700

    A few fixes here and there.

--- lib/auto/version.rb
+++ lib/auto/version.rb
@@ -3,25 +3,27 @@
 
 module Auto
   
+  # @return [Boolean] Whether this is a HEAD (i.e. Git) copy.
+  def self.head?
+    begin
+      return @head if defined? @head
+      @head = Dir.exists?(File.join(File.expand_path(File.join('..', '..', '..'), __FILE__), '.git'))
+    ensure
+      @head ||= false
+    end
+  end
+  
   # Standard version string.
   #
   # We use semantic versioning: +MAJOR.MINOR.PATCH.PRE.PRENUM+
   VERSION      = '4.0.0.alpha.1'.freeze
-
+  
   # Standard version plus the codename (assigned to each minor release).
   #
   # i.e., +VERSION-CODENAME+
-  FULLVERSION  = "#{VERSION}-phoenix".freeze
-
-  # @return [Boolean] Whether this is a HEAD (i.e. Git) copy.
-  def self.head?
-    begin
-      return HEAD if defined? HEAD
-      HEAD = Dir.exists?(File.join(File.expand_path(File.join('..', '..'), __FILE__), '.git'))
-    ensure
-      HEAD ||= false
-    end
-  end
+  FULLVERSION  = "#{VERSION}-phoenix"
+  FULLVERSION  << '-HEAD' if self.head?
+  FULLVERSION.freeze
 
   # @return [Boolean] Whether this is an alpha-stage copy.
   def self.alpha?
