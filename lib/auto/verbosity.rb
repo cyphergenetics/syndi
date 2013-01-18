5326679a0a1e048f373ead0e60f3ab51f0fbf7ce
noxgirl <autumn@destellae.net>
2013-01-18 00:29:42 -0700

    Adding verbosity constants.

---
+++ lib/auto/verbosity.rb
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, Autumn Perrault, et al. All rights reserved.
+# This free software is distributed under the FreeBSD license (LICENSE.md).
+
+$VERBOSITY = 0
+
+VSIMPLE = 1
+VUSEFUL = 2
+VNOISY  = 3
+
+# vim: set ts=4 sts=2 sw=2 et:
