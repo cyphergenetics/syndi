cyphergenetics/syndi

commit 4b35957f6b2e1cbd0508dca37e828fdeb85ef59d

Fixing a bug.

include/auto/libauto.h
@@ -30,6 +30,9 @@ extern VALUE ePluginError;
 #define OS_LINE_TERM "\n"
 #endif
 
+/* fetch the Auto directory */
+#define AUTO_DIR RSTRING_PTR(rb_gv_get("AUTO_DIR"))
+
 #endif
 
 /* vim: set ts=4 sts=4 sw=4 et cindent: */

ext/libauto/logger.c
@@ -45,7 +45,7 @@ static void log_out2scrn(int type, const char *message, int level)
  */
 static void log_out2file(const char *type, const char *message)
 {
-    char *log_file_name  = ALLOCA_N(char, MAX_TIME_STRING_LENGTH + 1);
+    char *log_file_name = ALLOCA_N(char, MAX_TIME_STRING_LENGTH + 1);
     char *log_time = ALLOCA_N(char, LOG_TIME_FORMAT_LENGTH + 1);
     FILE *log_file;
     
@@ -81,19 +81,28 @@ static void log_out2file(const char *type, const char *message)
 static void log_dircheck()
 {
     int result;
+    char *dir = ALLOCA_N(char, strlen(AUTO_DIR) + 5);
 
 #ifdef _WIN32
-    result = _mkdir("logs/");
+    sprintf(dir, "%s\\logs", AUTO_DIR);
+    result = _mkdir(dir);
 #else
-    result = mkdir("logs", S_IRWXU);
+    sprintf(dir, "%s/logs", AUTO_DIR);
+    result = mkdir(dir, S_IRWXU);
 #endif
 
     // Only raise an error if we fail to create the directory. 
     if (result != 0 && errno != EEXIST)
     {
         int error_number = errno;
-        rb_raise(eLogError, "Could not create logs/: %s", strerror(error_number));
+        rb_raise(eLogError, "Could not create %s: %s", dir, strerror(error_number));
     }
+
+#ifdef _WIN32
+    _chdir(dir);
+#else
+    chdir(dir);
+#endif
 }
 
 /* @overload initialize()

include/auto/logger.h
@@ -12,18 +12,21 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <time.h>
+#ifndef _WIN32
+#include <unistd.h>
+#endif
 
-// Set format based on target OS.
+/* Set format based on target OS. */
 #ifdef _WIN32
 #define LOG_FILE_FORMAT "logs\\%Y%m%d.log"
 #else
 #define LOG_FILE_FORMAT "logs/%Y%m%d.log"
 #endif
 
-// This is based on the desired output give or take a few characters.
+/* This is based on the desired output give or take a few characters. */
 #define MAX_TIME_STRING_LENGTH 18
 
-// ("YYYY-MM-DD HH:MM:SS +ZZZZ") "YEAR-MONTH-DAY HOUR:MINUTES:SECONDS UTC_OFFSET"
+/* ("YYYY-MM-DD HH:MM:SS +ZZZZ") "YEAR-MONTH-DAY HOUR:MINUTES:SECONDS UTC_OFFSET" */
 #define LOG_TIME_FORMAT_LENGTH 25
 
 extern VALUE cLogger;

commit a6250e1918ca3defbfeefb2f4a38e1170ba30851

Rectifying a segmentation fault introduced by 4b35957.

include/auto/libauto.h
@@ -31,7 +31,7 @@ extern VALUE ePluginError;
 #endif
 
 /* fetch the Auto directory */
-#define AUTO_DIR RSTRING_PTR(rb_gv_get("AUTO_DIR"))
+#define AUTO_DIR RSTRING_PTR(rb_const_get(rb_cObject, SYM(AUTO_DIR)))
 
 #endif
 