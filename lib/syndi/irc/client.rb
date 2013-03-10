460ffb4335c75afc1cd3b5c835b0ef62cb306c55
Autumn Perrault <autumn@destellae.net>
2013-03-10 16:11:09 -0600

    This would perhaps be more efficient CPU-wise.

--- lib/syndi/irc/client.rb
+++ lib/syndi/irc/client.rb
@@ -265,7 +265,7 @@ module Syndi
       def to_talk
         hash = Hash.new
         instance_variables.each do |var|
-          hash[var.sub(/^:@/, '')] = instance_variable_get var
+          hash[var[2..-1]] = instance_variable_get var
         end
         hash
       end
